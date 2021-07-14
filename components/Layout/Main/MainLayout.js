cc9c22717621f83a16d8a200ae5c9cd4ea7c32d5
Shen Sabado <stephanieshen.sabado@gmail.com>
2021-07-14 20:48:23 +0800

    navigation mobile view

--- components/Layout/Main/MainLayout.js
+++ components/Layout/Main/MainLayout.js
@@ -1,16 +1,28 @@
+import { route } from 'next/dist/next-server/server/router';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import Footer from '../../Footer/Footer';
 import Navigation from '../../Navigation/Navigation';
+import classes from './MainLayout.module.scss';
 
 const MainLayout = (props) => {
   const router = useRouter();
   const isContactPage = router.pathname.includes('contact');
 
+  const getBgColor = () => {
+    const path = router.pathname;
+    if (path.includes('about') ||
+        isContactPage) {
+          return '#1C2127';
+    }
+
+    return '#222831';
+  }
+
   return (
     <div>
-      <Navigation />
-      <main>
+      <Navigation bgColor={getBgColor()} />
+      <main className={classes.mainContent}>
         {props.children}
       </main>
       {!isContactPage && (
--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -1,9 +1,11 @@
 
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 import classes from './Navigation.module.scss';
+import { AccountCircleOutlined, DescriptionOutlined, EmailOutlined, Home, HomeOutlined, Phone } from '@material-ui/icons';
 
-const Navigation = () => {
+const Navigation = (props) => {
   const router = useRouter();
 
   const isActiveRoute = (path) => {
@@ -11,27 +13,64 @@ const Navigation = () => {
   }
 
   return (
-    <nav className={classes.navigation}>
+    <nav
+      className={classes.navigation}
+      style={{
+        backgroundColor: props.bgColor
+      }}
+    >
       <img
         className={classes.logo}
         src src="/assets/images/logo.png"
       />
       <ul className={classes.navList}>
         <li className={ isActiveRoute('/') }>
-          <Link href="/">Home</Link>
+          <Link href="/">
+            <div>
+              <HomeOutlined className={classes.linkIcon} />
+              <span className={classes.linkText}>
+                Home
+              </span>
+            </div>
+          </Link>
         </li>
         <li className={ isActiveRoute('/about') }>
-          <Link href="/about">About</Link>
+          <Link href="/about">
+            <div>
+              <AccountCircleOutlined className={classes.linkIcon} />
+              <span className={classes.linkText}>
+                About
+              </span>
+            </div>
+          </Link>
         </li>
         <li className={ isActiveRoute('/blogs') }>
-          <Link href="/blogs">Blogs</Link>
+          <Link href="/blogs">
+            <div>
+              <DescriptionOutlined className={classes.linkIcon} />
+              <span className={classes.linkText}>
+                Blogs
+              </span>
+            </div>
+          </Link>
         </li>
         <li className={ isActiveRoute('/contact') }>
-          <Link href="/contact">Contact</Link>
+          <Link href="/contact">
+            <div>
+              <EmailOutlined className={classes.linkIcon} />
+              <span className={classes.linkText}>
+                Contact
+              </span>
+            </div>
+          </Link>
         </li>
       </ul>
     </nav>
   );
+}
+
+Navigation.propTypes = {
+  bgColor: PropTypes.string
 }
 
 
