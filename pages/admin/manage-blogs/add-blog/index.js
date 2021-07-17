73545fd945ecc4dd5a761403abc3e17baad61776
Shen Sabado <stephanieshen.sabado@gmail.com>
2021-07-17 18:31:45 +0800

    add blog route

---
+++ pages/admin/manage-blogs/add-blog/index.js
@@ -0,0 +1,9 @@
+import classes from './AddBlog.module.scss';
+
+const AddBlog = () => {
+  return (
+    <p>Add Blog</p>
+  )
+}
+
+export default AddBlog;
--- pages/admin/manage-blogs/index.js
+++ pages/admin/manage-blogs/index.js
@@ -1,10 +1,27 @@
+import { useRouter } from 'next/dist/client/router';
 import BlogItems from '../../../components/BlogItems/BlogItems';
+import Button from '../../../components/Button/Button';
 import classes from './ManageBlogs.module.scss';
 
 const ManageBlogs = () => {
+  const router = useRouter();
+
+  const addBlog = () => {
+    router.push('/admin/manage-blogs/add-blog');
+  }
+
   return (
-    <div className={classes.blogCards}>
-      <BlogItems itemsEditable={true} />
+    <div>
+      <Button
+        type="button"
+        classes={['primary']}
+        clicked={addBlog}
+      >
+        Add Blog
+      </Button>
+      <div className={classes.blogCards}>
+        <BlogItems itemsEditable={true} />
+      </div>
     </div>
   )
 }
